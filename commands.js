03147ce5ea34a66c93a746a828b9eba20a71d9ac
eugene.bulkin <ebulkin@caltech.edu>
2014-03-09 23:27:46 -0500

    Quote alias

--- commands.js
+++ commands.js
@@ -78,6 +78,18 @@ var formatTransaction = function(info, players) {
   return str.join(" | ");
 };
 
+var quote = function(quotes, log) {
+  if(!(quotes instanceof Array)) {
+    quotes = [quotes];
+  }
+  return function(data, nick) {
+    if(log) {
+      this.log(log.split("$nick").join(nick));
+    }
+    this.talk(quotes[(Math.random() * quotes.length) | 0].replace("$nick", nick));
+  };
+};
+
 module.exports = function(get, chanName) {
   return {
     "reload": {
@@ -461,22 +473,13 @@ module.exports = function(get, chanName) {
       }
     },
     "leafer": {
-      fn: function(data, nick) {
-        this.log(nick + " quoted Leafer91");
-        this.talk("Man, I really could go for some throat lasagnas right now.");
-      }
+      fn: quote("Man, I really could go for some throat lasagnas right now.", "$nick quoted Leafer91")
     },
     "doubleaw": {
-      fn: function(data, nick) {
-        this.log(nick + " asked me to talk about how awesome DoubleAW is");
-        this.talk("That's the guy that made me. He must be way better than amaninacan.");
-      }
+      fn: quote("That's the guy that made me. He must be way better than amaninacan.", "$nick asked me to talk about how awesome DoubleAW is")
     },
     "ruhan": {
-      fn: function(data, nick) {
-        this.log(nick + " made fun of ruhan");
-        this.talk("No, you can't have the Chicago fourth line.");
-      }
+      fn: quote("No, ruhan, you can't have the Chicago fourth line.", "$nick made fun of ruhan")
     },
     "thero": {
       fn: function(data, nick) {
@@ -490,79 +493,56 @@ module.exports = function(get, chanName) {
       }
     },
     "dan": {
-      fn: function(data, nick) {
-        this.log(nick + " asked for a blowjob");
-        this.talk("Hey " + nick + " wanna suck me off?");
-      }
+      fn: quote("Hey $nick wanna suck me off?", "$nick asked for a blowjob")
     },
     "signal": {
-      fn: function(data, nick) {
-        var msgs = ["do you have a sister", "what color are your pubes", "would you drink my bathwater", "can you recommend a good porno"];
-        this.log(nick + " quoted SiGNAL");
-        this.talk("Hey " + nick + " " + msgs[(Math.random() * msgs.length) | 0] + "?");
-      }
+      fn: quote(["Hey $nick do you have a sister?", "Hey $nick what color are your pubes?", "Hey $nick would you drink my bathwater?", "Hey $nick can you recommend a good porno?"], "$nick quoted SiGNAL")
     },
     "uck": {
-      fn: function(data, nick) {
-        this.log(nick + " swore at me");
-        this.talk("Woah, watch your language, asshole.");
-      }
+      fn: quote("Woah, watch your language, asshole.", "$nick swore at me")
     },
     "thirty": {
-      fn: function(data, nick) {
-        this.log(nick + " quoted thirty");
-        this.talk("The fix will be posted at noon tomorrow on macrumors.com");
-      }
+      fn: quote("The fix will be posted at noon tomorrow on macrumors.com", "$nick quoted thirty")
     },
     "snackle": {
-      fn: function(data, nick) {
-        this.log(nick + " said something about Jasmine's nips");
-        this.talk("#JasminesNips2014");
-      }
+      fn: quote("#JasminesNips2014", "$nick said something about Jasmine's nips")
     },
     "panthers": {
-      fn: function(data, nick) {
-        this.log(nick + " blessed the channel with the Panthers logo");
-        this.talk("http://i.imgur.com/LXDNmml.jpg");
-      }
+      fn: quote("http://i.imgur.com/LXDNmml.jpg", "$nick blessed the channel with the Panthers logo")
+    },
+    "canucks": {
+      fn: quote("http://i.imgur.com/Pkqiwcj.jpg", "$nick made fun of the Canucks")
     },
     "source": {
-      fn: function(data, nick) {
-        this.log(nick + " asked about my source code");
-        this.talk("If you have suggestions/feature requests or just want to see the source, it's at http://github.com/eugene-bulkin/rhockeybot");
-      }
+      fn: quote("If you have suggestions/feature requests or just want to see the source, it's at http://github.com/eugene-bulkin/rhockeybot", "$nick asked about my source code")
     },
     "lahey": {
-      fn: function(data, nick) {
-        var msgs = [
-          "Birds of a shitfeather flock together, $nick.",
-          "You just opened Pandora's Shitbox, $nick.",
-          "I am the liquor.",
-          "The shit pool's gettin full $nick, time to strain the shit before it overflows. I will not have a Pompeiian shit catastrophe on my hands.",
-          "Did you see that, $nick? Goddamn shitapple driving the shitmobile. No body else in this park gives a fuck why should I?",
-          "The ole shit liner is coming to port, and I'll be there to tie her up.",
-          "How dare you involve my daughter in your hemisphere of shit.",
-          "Captain Shittacular.",
-          "I'm watching you, like a shithawk.",
-          "We're sailing into a shit typhoon $nick, we'd better haul in the jib before it gets covered in shit.",
-          "How dare you involve my daughter in your hemisphere of shit.",
-          "Your shit-goose is cooked, $nick.",
-          "Shit-apples never fall far from the shit-tree, $nick.",
-          "$nick's about to enter the shit tornado to Oz.",
-          "Do you feel that $nick? The way the shit clings to the air? Shit Blizzard.",
-          "Never Cry Shitwolf, $nick",
-          "Yes I used to drink, $nick, but I got the shitmonkey off my back for good.",
-          "You know what you get when two shit-tectonic plates collide? Shitquakes, $nick. Shitquakes.",
-          "The ole shit liner is coming to port, and I'll be there to tie her up.",
-          "We got the key to shitty city, $nick, and Julian's the muscular mayor.",
-          "You boys have loaded up a hair-trigger, double barrelled shitmachinegun, and the barrel's pointing right at your own heads.",
-          "Shit moths, $nick.",
-          "Do you know what a shit rope is, $nick?",
-          "The old shit barometer is rising."
-        ];
-        this.log(nick + " asked for a Lahey quote");
-        this.talk(msgs[(Math.random() * msgs.length) | 0].replace("$nick", nick));
-      }
+      fn: quote([
+        "Birds of a shitfeather flock together, $nick.",
+        "You just opened Pandora's Shitbox, $nick.",
+        "I am the liquor.",
+        "The shit pool's gettin full $nick, time to strain the shit before it overflows. I will not have a Pompeiian shit catastrophe on my hands.",
+        "Did you see that, $nick? Goddamn shitapple driving the shitmobile. No body else in this park gives a fuck why should I?",
+        "The ole shit liner is coming to port, and I'll be there to tie her up.",
+        "How dare you involve my daughter in your hemisphere of shit.",
+        "Captain Shittacular.",
+        "I'm watching you, like a shithawk.",
+        "We're sailing into a shit typhoon $nick, we'd better haul in the jib before it gets covered in shit.",
+        "How dare you involve my daughter in your hemisphere of shit.",
+        "Your shit-goose is cooked, $nick.",
+        "Shit-apples never fall far from the shit-tree, $nick.",
+        "$nick's about to enter the shit tornado to Oz.",
+        "Do you feel that $nick? The way the shit clings to the air? Shit Blizzard.",
+        "Never Cry Shitwolf, $nick",
+        "Yes I used to drink, $nick, but I got the shitmonkey off my back for good.",
+        "You know what you get when two shit-tectonic plates collide? Shitquakes, $nick. Shitquakes.",
+        "The ole shit liner is coming to port, and I'll be there to tie her up.",
+        "We got the key to shitty city, $nick, and Julian's the muscular mayor.",
+        "You boys have loaded up a hair-trigger, double barrelled shitmachinegun, and the barrel's pointing right at your own heads.",
+        "Shit moths, $nick.",
+        "Do you know what a shit rope is, $nick?",
+        "The old shit barometer is rising."
+      ], "$nick asked for a Lahey quote")
     }
   };
 };
